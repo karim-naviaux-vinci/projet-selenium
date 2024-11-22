be0bf87181326dcf8200a8650831cfaef71b4e55
ali beztout <ali.beztout@student.vinci.be>
2024-11-22 22:11:25 +0100

    ok

--- WebApp/src/App.jsx
+++ WebApp/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Counter from "./Components/Counter";
 import Component from './Components/Component'
 import Search from './Components/Search'
 
@@ -11,6 +12,7 @@ function App() {
     <div>
       <Component/>
       <Search items={["Pomme", "Mandarine", "Orange", "Raisin"]} />
+      <Counter /> 
     </div>
   )
 }
